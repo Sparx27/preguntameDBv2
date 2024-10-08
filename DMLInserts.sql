73203c8fbf6afc055d2ee14055f77682599431a1
sparx27 <nicolasgcardani@gmail.com>
2024-10-08 18:40:29 -0300

    update dmlinserts

--- DMLInserts.sql
+++ DMLInserts.sql
@@ -34,6 +34,14 @@ INSERT INTO Respuestas(PreguntaId, Dsc) VALUES
 ('B0CB0AAB-BD30-4A40-83AA-DDD3DC925A3B', 'Jugar en el campo con mis amigos durante el verano.'),
 ('D00E5457-FD5D-4EC4-9184-6D579B137458', 'Todo lo bueno que quieras lograr en la vida, va a requerir en menor o mayor medida de tu autodisciplina.')
 
+UPDATE Preguntas
+SET Estado = 1
+WHERE Preguntas.PreguntaId IN (
+	select p.PreguntaId
+	from Preguntas p
+	join Respuestas r on r.PreguntaId = p.PreguntaId
+)
+
 
 INSERT INTO Seguimientos(Usuario_Seguido, Usuario_Seguidor) VALUES
 ('9D021FE3-15A5-4C35-ADE1-324D5B16669D', 'BBAFDD56-A5DD-4118-A640-00CD9B773FEA'),
@@ -45,6 +53,13 @@ INSERT INTO Seguimientos(Usuario_Seguido, Usuario_Seguidor) VALUES
 ('650700AF-1D90-4BA6-A465-07173F606EB6', '9D021FE3-15A5-4C35-ADE1-324D5B16669D'),
 ('650700AF-1D90-4BA6-A465-07173F606EB6', 'BBAFDD56-A5DD-4118-A640-00CD9B773FEA'),
 ('650700AF-1D90-4BA6-A465-07173F606EB6', '8D7C73E8-620F-421C-963F-3968200E8EE7')
+
+UPDATE Usuarios
+SET NSeguidores = (
+	select count(s.Usuario_Seguido)
+	from Seguimientos s
+	where s.Usuario_Seguido = Usuarios.UsuarioId
+)
 
 
 INSERT INTO MeGustas(RespuestaId, UsuarioId) VALUES
@@ -58,3 +73,10 @@ INSERT INTO MeGustas(RespuestaId, UsuarioId) VALUES
 ('27A6A551-08B1-49A4-BA8C-8F5ABDAB1200', '0CA525FA-E384-4317-98B8-0C3E18485796'),
 ('27A6A551-08B1-49A4-BA8C-8F5ABDAB1200', '8D7C73E8-620F-421C-963F-3968200E8EE7'),
 ('1655C41C-B84C-4FCF-AEF0-B932AC877F2F', '0CA525FA-E384-4317-98B8-0C3E18485796')
+
+UPDATE Usuarios
+SET NLikes = (
+	select count(m.UsuarioId)
+	from MeGustas m
+	where m.UsuarioId = Usuarios.UsuarioId
+)
